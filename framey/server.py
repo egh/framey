f4f1ffbd72d57b07745eec08d4f06e19919d0130
Erik Hetzner <egh@e6h.org>
2023-06-15 18:00:14 -0700

    proper etags

--- framey/server.py
+++ framey/server.py
@@ -1,13 +1,19 @@
 import tempfile
 from framey import make_now_playing_card, dither_image
-from flask import Flask, make_response, send_file
+from flask import Flask, make_response, send_file, request
+from werkzeug.utils import send_file
+from zlib import adler32
 
 app = Flask(__name__)
 
 
 @app.route("/playing.jpeg")
 def now_playing():
-    tmpfile = tempfile.TemporaryFile(suffix=".jpeg")
+    tmpfile = tempfile.NamedTemporaryFile(suffix=".jpeg")
     make_now_playing_card().convert("RGB").save(tmpfile, format="JPEG")
     tmpfile.seek(0)
-    return send_file(tmpfile, mimetype="image/jpeg")
+    etag = str(adler32(tmpfile.read()) & 0xffffffff)
+    tmpfile.seek(0)
+    return send_file(tmpfile, request.environ, mimetype="image/jpeg",
+                     etag=etag)
+    
