b88baf679e20579d9a5399a3d5f790da522bd823
Erik Hetzner <egh@e6h.org>
2023-09-06 18:05:03 -0700

    Abstract out image serving code

--- framey/server.py
+++ framey/server.py
@@ -14,12 +14,16 @@ SCOPE = "user-library-read,user-read-currently-playing,user-read-recently-played
 app = Flask(__name__)
 
 
-@app.route("/playing.jpeg")
-def now_playing():
-    spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
+def serve_image(image):
     tmpfile = tempfile.NamedTemporaryFile(suffix=".jpeg")
-    make_now_playing_image(spotify_client).convert("RGB").save(tmpfile, format="JPEG")
+    image.convert("RGB").save(tmpfile, format="JPEG")
     tmpfile.seek(0)
     etag = str(adler32(tmpfile.read()) & 0xFFFFFFFF)
     tmpfile.seek(0)
     return send_file(tmpfile, request.environ, mimetype="image/jpeg", etag=etag)
+
+
+@app.route("/playing.jpeg")
+def now_playing():
+    spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
+    return serve_image(make_now_playing_image(spotify_client))
