89a460dbbb4f5cedd1e33e688ad7b37ac695af34
Erik Hetzner <egh@e6h.org>
2023-09-13 20:48:24 -0700

    Different endpoints based on button press

--- client/main.py
+++ client/main.py
@@ -15,9 +15,12 @@ from network_manager import NetworkManager
 from picographics import DISPLAY_INKY_FRAME_7 as DISPLAY  # 7.3"
 from picographics import PicoGraphics
 
-FILENAME = "/sd/playing.jpeg"
-FILENAME_ETAG = FILENAME + "etag"
-ENDPOINT = "http://retropie.local:5000/playing.jpeg"
+# Configure these
+ENDPOINT = "http://retropie.local:5000/"
+MAP = {inky_frame.button_a: "playing.jpeg", inky_frame.button_b: "weather.jpeg"}
+
+# Default button
+button = inky_frame.button_a
 
 
 def mount_sd_card():
@@ -31,9 +34,9 @@ def mount_sd_card():
     uos.mount(sdcard.SDCard(sd_spi, machine.Pin(22)), "/sd")
 
 
-def build_headers():
+def build_headers(filename):
     try:
-        with open(FILENAME_ETAG, "r") as f:
+        with open(filename + "_etag", "r") as f:
             print("reading etag")
             etag = f.read()
             return {"If-None-Match": etag}
@@ -49,13 +52,13 @@ def network_connect():
     )
 
 
-def read_jpeg(resp):
-    print("reading jpeg")
+def read_jpeg(resp, filename):
+    print("reading " + filename)
     socket = resp.raw
     # Stream the image data from the socket onto disk in 1024 byte chunks
     # the 600x448-ish jpeg will be roughly ~24k, we really don't have the RAM!
     data = bytearray(1024)
-    with open(FILENAME, "wb") as f:
+    with open(filename, "wb") as f:
         while True:
             if socket.readinto(data) == 0:
                 break
@@ -63,7 +66,7 @@ def read_jpeg(resp):
     socket.close()
 
 
-def display_jpeg():
+def display_jpeg(filename):
     print("displaying jpeg")
     graphics = PicoGraphics(DISPLAY)
     jpeg = jpegdec.JPEG(graphics)
@@ -71,37 +74,51 @@ def display_jpeg():
     graphics.set_pen(1)
     graphics.clear()
     gc.collect()
-    jpeg.open_file(FILENAME)
+    jpeg.open_file(filename)
     jpeg.decode(scale=False, dither=False)
     gc.collect()
     graphics.update()
 
 
-def write_etag(resp):
+def write_etag(resp, filename):
     if "ETag" in resp.headers:
         print("writing etag")
-        with open(FILENAME + "etag", "w") as f:
+        with open(filename + "_etag", "w") as f:
             f.write(resp.headers["ETag"])
 
 
 activity_led = machine.Pin(6, machine.Pin.OUT)
-
 try:
+    if inky_frame.button_a.read():
+        button = inky_frame.button_a
+    elif inky_frame.button_b.read():
+        button = inky_frame.button_b
+    elif inky_frame.button_c.read():
+        button = inky_frame.button_c
+    elif inky_frame.button_d.read():
+        button = inky_frame.button_d
+    elif inky_frame.button_e.read():
+        button = inky_frame.button_e
     inky_frame.pcf_to_pico_rtc()
     activity_led.on()
+    button.led_on()
     network_connect() and gc.collect()
     mount_sd_card() and gc.collect()
-    print("getting image")
-    resp = urequests.get(ENDPOINT, headers=build_headers())
+    image = MAP[button]
+    filename = "/sd/" + image
+    url = ENDPOINT + image
+    print("requesting " + url)
+    resp = urequests.get(url, headers=build_headers())
     if resp.status_code == 304:
         print("image unchanged")
     else:
-        read_jpeg(resp) and gc.collect()
-        display_jpeg() and gc.collect()
-        write_etag(resp) and gc.collect()
+        read_jpeg(resp, filename) and gc.collect()
+        display_jpeg(filename) and gc.collect()
+        write_etag(resp, filename) and gc.collect()
     gc.collect()
 except Exception as ex:
-    print(ex)
+    print("Error: " + ex)
 finally:
     activity_led.off()
+    button.led_off()
     inky_frame.sleep_for(5)
